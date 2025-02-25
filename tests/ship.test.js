96e4ee58d5094834e246aac623ade26edfc4a419
Tania <tania.wang@statista.com>
2025-02-25 11:34:00 +0100

    Add ship tests

---
+++ tests/ship.test.js
@@ -0,0 +1,41 @@
+const Ship = require("../src/shipFactory");
+const ship = require("../src/shipFactory");
+
+test("hello", () => {
+  expect("hello").toBe("hello");
+});
+
+test("Ship factory creates a ship", () => {
+  let ship = new Ship();
+  expect(ship).toBeDefined();
+});
+
+test("Adds hit to ship", () => {
+  let ship = new Ship();
+  ship.hit();
+  expect(ship.hitCount).toBe(1);
+});
+
+test("Sinks ship", () => {
+  let ship = new Ship();
+  ship.hit();
+  expect(ship.isSunk()).toBe(true);
+});
+
+test("Last hit, update sinks status", () => {
+  let ship = new Ship();
+  ship.hit();
+  expect(ship.sunk).toBe(true);
+});
+
+test.skip("Ship cannot have 0 length", () => {
+  let ship = new Ship(0);
+  expect(ship).toBeUndefined();
+});
+
+test("Hit count cannot exceed length", () => {
+  let ship = new Ship();
+  ship.hit();
+  ship.hit();
+  expect(ship.hitCount).toBe(1);
+});
