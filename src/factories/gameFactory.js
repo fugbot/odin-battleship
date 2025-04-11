a4d0ac64ca4f20230445e192637b5e8792cea868
Tania <tania.wang@statista.com>
2025-04-11 08:48:29 +0200

    Add game factory

---
+++ src/factories/gameFactory.js
@@ -0,0 +1,25 @@
+import Player from "./playerFactory.js";
+
+class Game {
+  constructor() {
+    this.player = new Player("player");
+    this.computer = new Player("computer");
+    this.currentPlayer = "player";
+  }
+
+  switchTurn() {
+    this.currentPlayer =
+      this.currentPlayer === "player" ? "computer" : "player";
+  }
+
+  checkGameOver() {
+    if (
+      this.player.gameboard.allShipsSunk() ||
+      this.computer.gameboard.allShipsSunk()
+    ) {
+      return true;
+    }
+  }
+}
+
+export default Game;
