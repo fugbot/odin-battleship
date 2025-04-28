3af4cab235fd1940682c6bc48e483a71792144cb
Tania <tania.wang@statista.com>
2025-04-28 13:44:58 +0200

    Add game initialization functionality

--- src/script.js
+++ src/script.js
@@ -1,37 +1,62 @@
 import {
-  createPlayerBoard,
-  createComputerBoard,
+  createCreateBoard,
   initGame,
   placeShipsRandomly,
+  playerPlacesShips,
 } from "./gamePlayController.js";
 
-/* document.addEventListener("DOMContentLoaded", () => {
-  initGameboards();
-}); */
-
+const gameboards = document.querySelector(".gameboards");
 const newBtn = document.querySelector("button#new-game");
-newBtn.addEventListener("click", () => {
-  document.getElementById("player-board").innerHTML = "";
 
-  document.getElementById("computer-board").innerHTML = "";
-
+document.addEventListener("DOMContentLoaded", () => {
   initGame();
-  placeShipsRandomly(); //todo: remove later just for testing
+  gameboards.style.display = "none";
+
   newBtn.style.display = "none";
 
-  //todo: add are you sure popup
-  //initGameboards()
+  createCreateBoard();
+  playerPlacesShips();
 });
 
+//click new button
+const sureDialog = document.querySelector("dialog.are-you-sure");
+const confirmBtn = document.querySelector("button.confirm");
+const cancelBtn = document.querySelector("button.cancel");
+
+newBtn.addEventListener("click", () => {
+  sureDialog.showModal();
+});
+
+confirmBtn.addEventListener("click", () => {
+  document.getElementById("player-board").innerHTML = "";
+  document.getElementById("computer-board").innerHTML = "";
+  gameboards.style.display = "none";
+
+  sureDialog.close();
+  initGame();
+  createCreateBoard();
+  playerPlacesShips();
+  newBtn.style.display = "none";
+});
+
+cancelBtn.addEventListener("click", () => {
+  sureDialog.close();
+});
+
+const rotateBtn = document.querySelector("button#rotate");
 const randomBtn = document.querySelector("button#random");
-const createBoard = document.querySelector(".create");
+const createSection = document.querySelector(".create");
+const createBoard = document.querySelector("#create-board");
 randomBtn.addEventListener("click", () => {
   initGame();
   console.log("Place ships randomly");
+  gameboards.style.display = "";
+  newBtn.style.display = "";
 
   //todo: if random clicked, clear gameboard objects
   placeShipsRandomly();
 
-  //remove create board elements
+  //remove create board divs & hide buttons
   createBoard.innerHTML = "";
+  createSection.style.display = "none";
 });
